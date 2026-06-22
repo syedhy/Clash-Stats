1873cf89b60ce944e2fe9b880871eb89b5ea4446
Syed Hyder <syedhyderalihamdani@gmail.com>
2026-06-22 16:41:08 +0530

    chore: stop tracking .env and dist directories

--- clash-companion-server/dist/test-cwl.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const clashApi_1 = require("./clashApi");
-async function test() {
-    const group = await (0, clashApi_1.getLeagueGroup)("#282YLR22C");
-    console.log(JSON.stringify(group, null, 2));
-}
-test();
